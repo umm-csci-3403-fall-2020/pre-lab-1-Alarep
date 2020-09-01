7b96b7e8a2e19628dee6a6f95ff28808e8440d85
Jack Perala <peralaj94@gmail.com>
2020-08-31 22:53:01 -0500

    Wrote the ability to use regex filtering on the r1_input file

--- regex/regex.sh
+++ regex/regex.sh
@@ -4,19 +4,8 @@
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt | tee r0_output.txt
 
 # Extracting the appropriate text for r1 file
-sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r1_input.txt | tee r1_output.txt
+awk 'match($0, /(\w+). My favorite sandwich is (\w+)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r1_input.txt | tee r1_output.txt
 
-# Extracting the appropriate text for r2 file
-# sed -E 's/\*([^[.+?].]), ([^[.+?].])/1. \1\n2. \2\n/' < r2_input.txt | tee r2_output.txt
 
-# Try again
+# Extracting the appropriate text for the r2 file
 sed -E 's/\* sandwich with ([a-zA-Z\.]+) ([a-zA-Z ]+)/1. \1\n2. \2\n/' < r2_input.txt | tee r2_output.txt
-
-# another
-# sed -E 's/\* ([\.[A-Z]+]), ([\.[A-Z]+)/1. \1\n2. \2\n/' < r2_input.txt | tee r2_output.txt
-
-# oof
-# awk 'match($0, /(^[a-z]+\.), (^[a-z]+\.)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r2_input.txt
-
-# awk attempt
-#awk -F '.' 'NF > 2 { sub("^[^.]*\.", "") } 1' r2_input.txt
