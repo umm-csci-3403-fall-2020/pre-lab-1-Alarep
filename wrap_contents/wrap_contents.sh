4f60141ec9f2638e8608a0f359ec778d07b1714a
Jack Perala <peralaj94@gmail.com>
2020-08-31 19:46:36 -0500

    Finished the wrap_contents script and created the my_chart html file

--- wrap_contents/wrap_contents.sh
+++ wrap_contents/wrap_contents.sh
@@ -5,6 +5,12 @@ targFile="$1"
 specifier="$2"
 targDest="$3"
 
-# 
-awk '/stop/{show=0} show; /start/ { show=1 }' $1
-output=$(sed -n '/start/,/stop/p' $1)
+# Creating the header and footer variables
+head="$specifier"_header.html
+foot="$specifier"_footer.html
+# I am not sure if I need these next two lines but I am leaving them in to ask questions
+#awk '/stop/{show=0} show; /start/ { show=1 }' "$1"
+#sed -n '/start/,/stop/p' "$1"
+
+# Combining the values
+cat "$head" "$targFile" "$foot" > "$targDest"
