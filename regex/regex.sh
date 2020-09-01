74cc5a3fdd4893b0842885f9a7a52446a12f94ca
Jack Perala <peralaj94@gmail.com>
2020-08-31 22:00:21 -0500

    Rewrote the third regex filter to pass

--- regex/regex.sh
+++ regex/regex.sh
@@ -7,10 +7,16 @@ sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt | tee r0_o
 sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r1_input.txt | tee r1_output.txt
 
 # Extracting the appropriate text for r2 file
-sed -E 's/\*([^[.+?].]), ([^[.+?].])/1. \1\n2. \2\n/' < r2_input.txt | tee r2_output.txt
+# sed -E 's/\*([^[.+?].]), ([^[.+?].])/1. \1\n2. \2\n/' < r2_input.txt | tee r2_output.txt
 
 # Try again
-sed '/\..*\./s/^[^.]*\.//' r2_input.txt
+sed -E 's/\* sandwich with ([a-zA-Z\.]+) ([a-zA-Z ]+)/1. \1\n2. \2\n/' < r2_input.txt | tee r2_output.txt
+
+# another
+# sed -E 's/\* ([\.[A-Z]+]), ([\.[A-Z]+)/1. \1\n2. \2\n/' < r2_input.txt | tee r2_output.txt
+
+# oof
+# awk 'match($0, /(^[a-z]+\.), (^[a-z]+\.)/, groups) {print "1. " groups[1] "\n" "2. " groups[2] "\n" }' < r2_input.txt
 
 # awk attempt
-awk -F '.' 'NF > 2 { sub("^[^.]*\.", "") } 1' r2_input.txt
+#awk -F '.' 'NF > 2 { sub("^[^.]*\.", "") } 1' r2_input.txt
